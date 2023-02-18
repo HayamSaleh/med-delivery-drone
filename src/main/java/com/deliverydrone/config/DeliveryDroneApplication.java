6b6223403faf334be581b5445bbde9cf0200c59f
HayamSaleh <50534751+HayamSaleh@users.noreply.github.com>
2023-02-18 19:42:45 +0200

    Add mapper and mapperUtils

--- src/main/java/com/deliverydrone/config/DeliveryDroneApplication.java
+++ src/main/java/com/deliverydrone/config/DeliveryDroneApplication.java
@@ -1,8 +1,10 @@
 package com.deliverydrone.config;
 
+import org.dozer.DozerBeanMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,6 +16,11 @@ public class DeliveryDroneApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DeliveryDroneApplication.class, args);
+  }
+
+  @Bean
+  public DozerBeanMapper dozerBeanMapper() {
+    return new DozerBeanMapper();
   }
 
 }
