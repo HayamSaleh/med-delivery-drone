c318d5239a7e1ed71bcf767d29321e33524e74f7
HayamSaleh <50534751+HayamSaleh@users.noreply.github.com>
2023-02-18 14:44:39 +0200

    project initialization

---
+++ src/main/java/com/deliverydrone/DeliveryDroneApplication.java
@@ -0,0 +1,13 @@
+package com.deliverydrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDroneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/deliverydrone/controller/DroneController.java
@@ -0,0 +1,20 @@
+package com.deliverydrone.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/drones")
+public class DroneController {
+
+	@GetMapping()
+	public ResponseEntity<?> getAllDrones() {
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+	}
+
+}
